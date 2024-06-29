34d6a835c27d260885fd39cb2dd9ed501ae1eca9
adinath <adinathsyadav2016@gmail.com>
2024-06-29 22:01:56 +0530

    I have prepare home page with basic things

---
+++ script.js
@@ -0,0 +1,11 @@
+function openInstagram( ){
+    window.open("https://www.instagram.com/adinath.yadav.2002/", "_blank")
+}
+
+function openLinkedin( ){
+    window.open("https://www.linkedin.com/in/adinath-yadav-50a294251/", "_blank")
+}
+
+function openGithub( ){
+    window.open("https://github.com/adinathyadav2002", "_blank")
+}
